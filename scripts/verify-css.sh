fa92cf57663f55b692a2b3e03eb614f0b69ef0e1
Jonathan Shaffer <jon@hyperfluidsolutions.com>
2025-02-13 09:19:01 -0500

    chore: fixing references in verify css

--- scripts/verify-css.sh
+++ scripts/verify-css.sh
@@ -2,11 +2,11 @@
 
 echo "Finding CSS files..."
 # Find all .css files except those in node_modules.
-files=$(find . -type f -name "*.css" -not -path "*/node_modules/*")
+files=$(find . -type f -name "*.css" -not -path "*/node_modules/*" -not -path "./docs/*")
 exit_code=0
 for file in $files; do
   echo "Verifying $file ..."
-  node test/verify-css.js "$file" || exit_code=1
+  node scripts/verify-css.js "$file" || exit_code=1
 done
 if [ $exit_code -ne 0 ]; then
   echo "One or more CSS files are not entirely wrapped in '@media print'."
